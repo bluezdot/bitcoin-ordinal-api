f2fc7cca005aafbb0fa829a61ef4eb8f563b97d1
bluezdot <72647326+bluezdot@users.noreply.github.com>
2023-12-20 12:32:29 +0700

    add apis list

---
+++ constant.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HIRO_APIs = void 0;
+exports.HIRO_APIs = {
+    api_status: 'https://api.hiro.so/ordinals/v1/',
+    list_of_incriptions: 'https://api.hiro.so/ordinals/v1/inscriptions',
+    transfers_per_block: 'https://api.hiro.so/ordinals/v1/inscriptions/transfers',
+    specific_inscription: 'https://api.hiro.so/ordinals/v1/inscriptions/:id',
+    inscription_content: 'https://api.hiro.so/ordinals/v1/inscriptions/:id/content',
+    inscription_transfers: 'https://api.hiro.so/ordinals/v1/inscriptions/:id/transfers',
+    satoshi_ordinal: 'https://api.hiro.so/ordinals/v1/sats/:ordinal',
+    satoshi_inscriptions: 'https://api.hiro.so/ordinals/v1/sats/:ordinal/inscriptions',
+    inscription_count_per_block: 'https://api.hiro.so/ordinals/v1/stats/inscriptions',
+    brc20_tokens: 'https://api.hiro.so/ordinals/v1/brc-20/tokens',
+    brc20_token_details: 'https://api.hiro.so/ordinals/v1/brc-20/tokens/:ticker',
+    brc20_token_holders: 'https://api.hiro.so/ordinals/v1/brc-20/tokens/:ticker/holders',
+    brc20_balances: 'https://api.hiro.so/ordinals/v1/brc-20/balances/:address',
+    brc20_activity: 'https://api.hiro.so/ordinals/v1/brc-20/activity'
+};
